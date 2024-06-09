0bfe896b5e5842137336102045756d828ece8a10
Angel Borrego Marron <angelbormar16@gmail.com>
2024-06-09 04:12:13 +0200

    3

--- src/app/incidencias/edit/[id]/page.js
+++ src/app/incidencias/edit/[id]/page.js
@@ -1,28 +1,28 @@
-// app/incidencias/edit/[id]/page.js
+// // app/incidencias/edit/[id]/page.js
 
-import { notFound } from 'next/navigation';
-import IncidenciaForm from '../../../../components/IncidenciaForm';
-import prisma from '../../../../lib/prisma';
+// import { notFound } from 'next/navigation';
+// import IncidenciaForm from '../../../../components/IncidenciaForm';
+// import prisma from '../../../../lib/prisma';
 
-const EditarIncidenciaPage = async ({ params }) => {
-  const { id } = params;
+// const EditarIncidenciaPage = async ({ params }) => {
+//   const { id } = params;
 
-  // Obtener la incidencia desde la base de datos
-  const incidencia = await prisma.incidencia.findUnique({
-    where: { id: parseInt(id, 10) },
-  });
+//   // Obtener la incidencia desde la base de datos
+//   const incidencia = await prisma.incidencia.findUnique({
+//     where: { id: parseInt(id, 10) },
+//   });
 
-  // Si no se encuentra la incidencia, mostrar la página 404
-  if (!incidencia) {
-    notFound();
-  }
+//   // Si no se encuentra la incidencia, mostrar la página 404
+//   if (!incidencia) {
+//     notFound();
+//   }
 
-  return (
-    <div>
-      <h1>Editar Incidencia</h1>
-      <IncidenciaForm initialData={incidencia} />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>Editar Incidencia</h1>
+//       <IncidenciaForm initialData={incidencia} />
+//     </div>
+//   );
+// };
 
-export default EditarIncidenciaPage;
+// export default EditarIncidenciaPage;
