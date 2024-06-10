87b3d4a5b94453dba081f1f18cd7d232beb0dd1e
Angel Borrego Marron <angelbormar16@gmail.com>
2024-06-10 14:40:24 +0200

    7

--- src/app/incidencias/delete/page.js
+++ src/app/incidencias/delete/page.js
@@ -1,23 +1,23 @@
-import Form from "@/components/FormIncidencia"
-import Button from "@/components/Button"
-import { prisma } from '@/lib/prisma'
-import { deleteIncidencia } from "@/lib/actions"
+// import Form from "@/components/FormIncidencia"
+// import Button from "@/components/Button"
+// import { prisma } from '@/lib/prisma'
+// import { deleteIncidencia } from "@/lib/actions"
 
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
 
-async function page({ searchParams }) {
-  const incidencia = await prisma.incidencia.findUnique({
-    where: {
-      id: Number(searchParams.id),
-    },
-  });
+// async function page({ searchParams }) {
+//   const incidencia = await prisma.incidencia.findUnique({
+//     where: {
+//       id: Number(searchParams.id),
+//     },
+//   });
 
-  return (
-    <div style={{ minHeight: "93vh", backgroundImage: "url('/frutas-y-frutos-secos.jpg')", backgroundSize: "cover", backgroundPosition: "center" }}>
-      <h3 className="text-3xl font-bold text-center">Eliminar incidencia {searchParams.id}</h3>
-      <Form action={deleteIncidencia} title='Eliminar incidencia' producto={incidencia} />
-    </div>
-  )
-}
+//   return (
+//     <div style={{ minHeight: "93vh", backgroundImage: "url('/frutas-y-frutos-secos.jpg')", backgroundSize: "cover", backgroundPosition: "center" }}>
+//       <h3 className="text-3xl font-bold text-center">Eliminar incidencia {searchParams.id}</h3>
+//       <Form action={deleteIncidencia} title='Eliminar incidencia' producto={incidencia} />
+//     </div>
+//   )
+// }
 
-export default page
+// export default page
